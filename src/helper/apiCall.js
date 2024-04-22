6ab9f339a826fe8e3af2938256d266c7996879b4
Jennifer Ezediuno <105203495+Uchediuno@users.noreply.github.com>
2024-04-22 21:53:09 +0100

    Update apiCall.js

--- src/helper/apiCall.js
+++ src/helper/apiCall.js
@@ -1,22 +1,5 @@
 import axios from "axios";
 
-// const githubAPI = axios.create({
-// 	baseURL: "https://api.github.com",
-// 	headers: {
-// 		Authorization: "token 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",
-// 	},
-// });
-
-// githubAPI
-// 	.get("/user/repos")
-// 	.then((response) => {
-// 		const repositories = response.data;
-// 		console.log(repositories);
-// 	})
-// 	.catch((error) => {
-// 		console.error("Error fetching repositories:", error);
-// 	});
-
 export const getRepositories = async () => {
 	try {
 		const res = await axios.get("https://api.github.com/users/uchediuno/repos", {
