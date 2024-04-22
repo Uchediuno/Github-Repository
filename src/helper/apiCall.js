d87cb74673125ad7ccfad4392e0db8b1706bbfa5
unknown <ucheezediuno@gmail.com>
2024-04-22 22:49:02 +0100

    Changes to access token

--- src/helper/apiCall.js
+++ src/helper/apiCall.js
@@ -4,7 +4,7 @@ export const getRepositories = async () => {
 	try {
 		const res = await axios.get("https://api.github.com/users/uchediuno/repos", {
 			headers: {
-				Authorization: "token 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",
+				Authorization: "token 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"
 			},
 		});
 		return res.data
