5b9318d85f71a4e5af35827c73c3ef53786d352f
unknown <ucheezediuno@gmail.com>
2024-04-23 13:38:32 +0100

    added dotenv

--- src/helper/apiCall.js
+++ src/helper/apiCall.js
@@ -4,7 +4,7 @@ export const getRepositories = async () => {
 	try {
 		const res = await axios.get("https://api.github.com/users/uchediuno/repos", {
 			headers: {
-				Authorization: "token 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"
+				Authorization: `token ${import.meta.env.VITE_GITHUB_TOKEN}`,
 			},
 		});
 		return res.data
